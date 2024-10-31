b091d1ccfc10f36dcc179f9cd9d534b9f0eb1cac
ctroubit <ctroubit@uwo.ca>
2024-10-31 14:16:39 -0400

    Added game board

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import pygame
 import sys
 
 from const import *
+from game import Game
 
 class Main:
 
@@ -9,10 +10,13 @@ class Main:
         pygame.init()
         self.screen = pygame.display.set_mode((WIDTH,HEIGHT))
         pygame.display.set_caption('stockshrimp')
+        self.game = Game()
+
 
 
     def mainloop(self):
         while True:
+            self.game.show_bg(self.screen)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
